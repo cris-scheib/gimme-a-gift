e977486b6aa60a4c7b07e72ccac6727adb3cdd3d
cris-scheib <cris.scheib@hotmail.com>
2022-04-05 22:16:12 -0300

    Criado front e back da lista de presentes

---
+++ api/src/routes/lists.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import Middleware from '../middleware';
+
+import ListController from '../controllers/listController';
+const listController = new ListController();
+
+const router = express.Router();
+
+router.get('/lists', Middleware.validateToken, listController.index);
+router.post('/lists', Middleware.validateToken, listController.create);
+router.get('/lists/:id', Middleware.validateToken, listController.show);
+router.patch('/lists/:id', Middleware.validateToken, listController.update);
+router.delete('/lists', Middleware.validateToken, listController.destroy);
+
+export { router as listRouter };
--- api/src/routes/routes.ts
+++ api/src/routes/routes.ts
@@ -3,10 +3,12 @@ import express from 'express';
 import { homeRouter } from './home';
 import { authRouter } from './auth';
 import { userRouter } from './user';
+import { listRouter } from './lists';
 
 const routes = express.Router();
 routes.use(homeRouter);
 routes.use(authRouter);
 routes.use(userRouter);
+routes.use(listRouter);
 
 export default routes;
